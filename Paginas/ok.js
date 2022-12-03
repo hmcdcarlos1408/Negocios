c9249a05f186b64f6f3e8600a8d7bd8d3b5879d6
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-03 16:57:34 -0600

    first commit

--- Paginas/ok.js
+++ Paginas/ok.js
@@ -1,5 +1,17 @@
+const tabla =document.querySelector('#lista-usuarios tbody');
 function cargar(){
    fetch('ok.json')
    .then(respuesta=>respuesta.json())
-   .then(respuesta=>console.log(respuesta))
+   .then(usuarios=>{
+    usuarios.forEach(usuarios=>{
+        const row=document.createElement('tr');
+        row.innerHTML+= `
+        <td>${usuarios.usuarios}</td>
+        <td>${usuarios.clave}</td>
+        `;
+        tabla.appendChild(row);
+        t
+    })
+   })
+
 }cargar();
