c7005fd9fba201b2b339997e22a9d88c0f49cb9a
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-03 18:52:55 -0600

    first commit

--- Paginas/ok.js
+++ Paginas/ok.js
@@ -1,17 +1,35 @@
-const tabla =document.querySelector('#lista-usuarios tbody');
-function cargar(){
-   fetch('ok.json')
-   .then(respuesta=>respuesta.json())
-   .then(usuarios=>{
-    usuarios.forEach(usuarios=>{
-        const row=document.createElement('tr');
-        row.innerHTML+= `
-        <td>${usuarios.usuarios}</td>
-        <td>${usuarios.clave}</td>
-        `;
-        tabla.appendChild(row);
-        t
-    })
-   })
+const tabla = document.querySelector('#lista-usuarios tbody');
 
-}cargar();
+function cargarUsuarios() {
+    fetch('usuarios.json')
+        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                const row = document.createElement('tr');
+                row.innerHTML += `
+                    <td>${usuario.id}</td>
+                    <td>${usuario.name}</td>
+                    <td>${usuario.username}</td>
+                 
+                `;
+                tabla.appendChild(row);
+
+                // const card = document.createElement('card');
+                // card.classList = 'card';
+                // card.innerHTML += `
+                //                 <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="width:100%">
+                //                     <div class="container">
+                //                         <h4><b>${usuario.id}</b></h4>
+                //                         <p>${usuario.name}</p>
+                //                     </div>
+                            
+                //             `;
+
+                // tabla.appendChild(card);
+
+            });
+        }) // Aquí mostramos dicha información
+        .catch(error => console.log('Hubo un error : ' + error.message))
+}
+
+cargarUsuarios();
