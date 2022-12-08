38de915739511b8b3e7b23be30dbd4e4a4c69106
FerEmi <90599744+FerEmilene@users.noreply.github.com>
2022-12-08 00:43:29 -0600

    Update UserC.php

--- Paginas/UserC.php
+++ Paginas/UserC.php
@@ -4,7 +4,6 @@ $name=$_POST['Name'];
 $username=$_POST['Username'];
 $pass=$_POST['Pass'];
 $inser="INSERT INTO `usuarios` VALUES ('$name','$username','$pass','cliente','')";
-
 $res=mysqli_query($conn,$inser);
 mysqli_close($conn);
 header("location:Login.html")
