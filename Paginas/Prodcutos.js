00b3a8b7d55e3ed350bc486b43f5fc60d0f8b372
hmcdcarlos1408 <117197233+hmcdcarlos1408@users.noreply.github.com>
2022-12-08 12:05:12 -0600

    master

--- Paginas/Prodcutos.js
+++ Paginas/Prodcutos.js
@@ -8,9 +8,9 @@ function cargarUsuarios() {
                
 
                 const card = document.createElement('card');
-                card.classList = 'card';
+                card.classList = 'card'; 
                 card.innerHTML += `
-                                <img src="../${usuario.id}" alt="Avatar" style="width:100%">
+                                <img src="${usuario.id}" alt="" style="width:100%">
                                     <div class="container">
                                         <h4><b>${usuario.product}</b></h4>
                                         <p>${usuario.categoria}</p>
