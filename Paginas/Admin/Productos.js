84184ca5d359c30a220dd0cfad21688c05a62657
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-12 18:20:28 -0600

    master

--- Paginas/Admin/Productos.js
+++ Paginas/Admin/Productos.js
@@ -16,10 +16,13 @@ function cargarUsuarios() {
                     <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Editar
                     </button>
+                    </td>
+                    <td>
                     <button type="button" class="btn btn-danger">
                     Eliminar
                     </button>
-                    </button>
+                    </td>
+                    <td>
                     <button type="button" class="btn btn-success">
                     Agregar
                     </button>
--- Paginas/Admin/Sales.js
+++ Paginas/Admin/Sales.js
@@ -15,6 +15,8 @@ function cargarUsuarios() {
                     <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Editar
                     </button>
+                    </td>
+                    <td>
                     <button type="button" class="btn btn-danger">
                     Eliminar
                     </button>
