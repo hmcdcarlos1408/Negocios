d238f7aebd68d06d9373beaf75a1e1968c1b067a
FerEmi <90599744+FerEmilene@users.noreply.github.com>
2022-12-12 23:26:59 -0600

    Update Productos.js

--- Paginas/Admin/Productos.js
+++ Paginas/Admin/Productos.js
@@ -1,5 +1,4 @@
 const tabla = document.querySelector('#lista-usuarios tbody');
-
 function cargarUsuarios() {
     fetch('Json/productos.json')
         .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
@@ -34,3 +33,6 @@ function cargarUsuarios() {
         .catch(error => console.log('Hubo un error: ' + error.message))
 }
 cargarUsuarios();
+
+
+
