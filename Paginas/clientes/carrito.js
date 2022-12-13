1546b49ffa9791052fc0732cc1ee73049c425800
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-12 23:54:56 -0600

    master

--- Paginas/clientes/carrito.js
+++ Paginas/clientes/carrito.js
@@ -6,10 +6,10 @@ function cargarUsuarios() {
             usuarios.forEach(usuario => {
                 const row = document.createElement('tr');
                 row.innerHTML += `
-                    <td>${usuario.id}</td>
-                    <td>${usuario.product}</td>
-                    <td>${usuario.des}</td>
-                    <td>${usuario.costo}</td>
+                <td>${usuario.id}</td>
+                <td>${usuario.product}</td>
+                <td>${usuario.des}</td>
+                <td>${usuario.costo}</td>
                     <td>
                     <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete">
                     Eliminar prodcutos
