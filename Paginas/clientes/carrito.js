893a6c88f08c6c8f090b9c86dbc959b405a59e5e
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-12 23:38:40 -0600

    master

---
+++ Paginas/clientes/carrito.js
@@ -0,0 +1,26 @@
+const tabla = document.querySelector('#lista-usuarios tbody');
+
+function cargarUsuarios() {
+    fetch('carrito.json')
+        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                const row = document.createElement('tr');
+                row.innerHTML += `
+                    <td>${usuario.id}</td>
+                    <td>${usuario.product}</td>
+                    <td>${usuario.des}</td>
+                    <td>${usuario.costo}</td>
+                    <td>
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#delete">
+                    Eliminar prodcutos
+                    </button>
+                    </td>
+                
+                `;
+                tabla.appendChild(row);
+            });
+        }) // Aquí mostramos dicha información
+        .catch(error => console.log('Hubo un error: ' + error.message))
+}
+cargarUsuarios();
