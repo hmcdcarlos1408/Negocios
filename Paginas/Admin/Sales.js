b9f21f5bc0b4f5811cce064eef6f1fe58a3afdb1
FerEmi <90599744+FerEmilene@users.noreply.github.com>
2022-12-12 23:28:00 -0600

    Update Sales.js

--- Paginas/Admin/Sales.js
+++ Paginas/Admin/Sales.js
@@ -1,5 +1,4 @@
 const tabla = document.querySelector('#lista-usuarios tbody');
-
 function cargarUsuarios() {
     fetch('Json/Sales.json')
         .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
@@ -23,10 +22,11 @@ function cargarUsuarios() {
                     </td>
                 `;
                 tabla.appendChild(row);
-                
             });
         }) // Aquí mostramos dicha información
         .catch(error => console.log('Hubo un error: ' + error.message))
 }
-
 cargarUsuarios();
+
+
+
