3fa258940715b7821680d50a3febaa282b11c93b
hmcdcarlos1408 <hmcdcarlos@outlook.es>
2022-12-12 18:08:09 -0600

    master

--- Paginas/Admin/Sales.js
+++ Paginas/Admin/Sales.js
@@ -11,6 +11,14 @@ function cargarUsuarios() {
                     <td>${usuario.Total}</td>
                     <td>${usuario.Name_user}</td>
                     <td>${usuario.Desc}</td>
+                    <td>
+                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Editar
+                    </button>
+                    <button type="button" class="btn btn-danger">
+                    Eliminar
+                    </button>
+                    </td>
                 `;
                 tabla.appendChild(row);
                 
